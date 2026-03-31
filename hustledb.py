c02a00d2b7d8e25b14cdc06afc7da3c94a435a94
swiftguy-1 <brightkelechi@gmail.com>
2026-04-01 00:01:45 +0100

    finetunnedAPI

--- hustledb.py
+++ hustledb.py
@@ -1,23 +1,38 @@
 import os
 import mailer
-from fastapi import FastAPI
-from pydantic import BaseModel
+import logging
+import traceback
+from fastapi import Request
+from slowapi import Limiter
+from slowapi.util import get_remote_address
+from fastapi import FastAPI,BackgroundTasks
+from fastapi import HTTPException
+from pydantic import BaseModel,EmailStr
 from dotenv import load_dotenv
-from fastapi import BackgroundTasks
 from supabase import create_client, Client
 from fastapi.middleware.cors import CORSMiddleware
+from postgrest.exceptions import APIError
 
+limiter = Limiter(key_func = get_remote_address)
+
+logging.basicConfig(
+    filename="database_errors.log",
+    level = logging.ERROR,
+    format = "%(asctime)s - %(levelname)s - %(message)s"
+)
 
 load_dotenv(dotenv_path=".env")
 
 app=FastAPI()
+
+app.state.limiter = limiter
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_ANON_KEY = os.getenv("SUPABASE_ANON_KEY")
 ALLOWED_ORIGIN = os.getenv("ALLOWED_ORIGINS")
 
 print(f"connecting... to {SUPABASE_URL}")
-print(f"accessing supabase with anon key: {SUPABASE_ANON_KEY[20:30]}*****end...")
+print(f"accessing supabase with anon key: {SUPABASE_ANON_KEY[20:30]}*****END...")
 print("successfully connected to SUPABASE!")
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_ANON_KEY) #supabase client instance
@@ -35,25 +50,56 @@ app.add_middleware(
 )
 
 class waitlist(BaseModel):
+    '''Schema for Validating client input'''
     name:str
-    email:str
+    email:EmailStr
     
-# Waitlist endpoint or route
+# route
 @app.post("/waitlist")
+@limiter.limit("5/minute")
+def reg(request: Request, user_details: waitlist, background_tasks: BackgroundTasks):
+    try:
+        strip_email = user_details.email.strip().lower() 
+        strip_name = user_details.name.strip()
+     
+        response1=supabase.table("Api_waitlist_table").select("email").eq("email", strip_email).execute()
 
-def reg(user_details: waitlist, background_tasks: BackgroundTasks):
-  data_dict = user_details.model_dump()
-  response=supabase.table("Api_waitlist_table").insert(data_dict).execute()
-  
-  # Terminal feedback
-  print(f'{user_details.name} has joined the waitlist!')
-  
-  # Email message running in the background for limited process time. 
-  background_tasks.add_task(
-      mailer.send_welcome_message,
-      user_details.email,
-      user_details.name
-  )
-  print(response)
-  # Browser feedback
-  return {"feedback": 'You have been added to the waitlist!'}
+        if len(response1.data) > 0:
+            return {"Feedback":  "Email is already taken. Try another email"}
+
+        response2=supabase.table("Api_waitlist_table").select("name").eq("name", strip_name).execute()
+
+        if len(response2.data) > 0:
+            return {"Feedback": "Name already taken. Try another one."}
+
+        data_json = user_details.model_dump()
+        db_response = supabase.table("Api_waitlist_table").insert(data_json).execute()
+
+        print(f"{user_details.name}, just joined the waitlist.")
+
+        background_tasks.add_task(
+            mailer.send_welcome_message,
+            user_details.email,
+            user_details.name
+        )
+
+        print(db_response.data)
+        return {"Feedbck": "You successfully joined the waitlist!"}
+        
+    except HTTPException as http_err:
+        raise http_err
+
+    except APIError as db_err:
+        logging.error(f"DATABASE ERROR: {db_err}")
+        raise HTTPException (
+            status_code = 500,
+            detail= "Database error, Try again later."
+        )   
+
+    except Exception as e:
+        logging.error(f"UNEXPECTED SYSTEM ERROR: {e}")
+        traceback.print_exc()
+        raise HTTPException (
+            status_code = 500,
+            detail = "Something went wrong on our end. This is not your fault."
+        )
